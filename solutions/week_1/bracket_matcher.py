b094ce73af2e73395a683c1bab0b72277022870c
Omar Essam <essamomar528@gmail.com>
2023-05-08 16:26:37 +0300

    BracketMatcher

--- solutions/week_1/bracket_matcher.py
+++ solutions/week_1/bracket_matcher.py
@@ -1,27 +1,44 @@
-def BracketMatcher(str):
-    counter1 = 0
-    counter2 = 0
-    counter3 = 0
-    if not str or str == " ":
-        return False
-    else:
-        for i in str:
-            if i == "(":
-                counter1 += 1
-            elif i == ")":
-                counter1 -= 1
-            elif i == "{":
-                counter2 += 1
-            elif i == "}":
-                counter2 -= 1
-            elif i == "[":
-                counter3 += 1
-            elif i == "]":
-                counter3 -= 1
-    if counter1 == 0 and counter2 == 0 and counter3 == 0:
-        return True
-    else:
-        return False
+def BracketMatcher(str) -> bool:
+    Map = {")": "(", "]": "[", "}": "{"}
+    stack = []
+
+    for c in str:
+        if c not in Map:
+            stack.append(c)
+            continue
+        if not stack or stack[-1] != Map[c]:
+            return "NO"
+        stack.pop()
+
+    return "YES"
 
 
-print(BracketMatcher("([)]"))
+print(BracketMatcher("[([)]"))
+
+# def BracketMatcher(str):
+#    counter1 = 0
+#    counter2 = 0
+#    counter3 = 0
+#    if not str or str == " ":
+#        return False
+#    else:
+#        for i in str:
+#            if i == "(":
+#                counter1 += 1
+#            elif i == ")":
+#                counter1 -= 1
+#            elif i == "{":
+#                counter2 += 1
+#            elif i == "}":
+#                counter2 -= 1
+#            elif i == "[":
+#                counter3 += 1
+#            elif i == "]":
+#                counter3 -= 1
+#    if counter1 == 0 and counter2 == 0 and counter3 == 0:
+#        return True
+#    else:
+#        return False
+
+
+# print(BracketMatcher("([)]"))
